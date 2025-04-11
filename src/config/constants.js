c57b764f10194e5ede4ea637a61e748bed1435e6
JefferDevop <somadesarrollo@gmail.com>
2025-04-11 16:39:57 -0500

    Change mayoristas on minoristas

--- src/config/constants.js
+++ src/config/constants.js
@@ -9,6 +9,6 @@ export const SEARCH_HISTORY = "search-history";
 export const CART = "cart_zp";
 export const CARTPRODUCTS = "cart_produc_zp";
 export const TOKEN = "token_zp";
-export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/"
-// export const BASE_NAME = "https://res.cloudinary.com/ciberneto/"
+// export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/"
+export const BASE_NAME = "https://res.cloudinary.com/ciberneto/"
 
