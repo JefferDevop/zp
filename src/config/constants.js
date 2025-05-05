a25557b3138b8e4bb7a7c27049d24517d8f15f7a
JefferDevop <somadesarrollo@gmail.com>
2025-05-04 19:29:29 -0500

    Manejo de talla

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,14 +1,12 @@
-export const BASE_API = "http://desarrollo.localhost:8000" 
+// export const BASE_API = "http://desarrollo.localhost:8000" 
 // export const BASE_API = "https://boutiquemonaco.workhard.site"
 
-// export const BASE_API = "https://zp.soluciones.today"
-
-// export const BASE_API = "https://antotex.soluciones.space"
+export const BASE_API = "https://zp.soluciones.today"
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart_zp";
 export const CARTPRODUCTS = "cart_produc_zp";
 export const TOKEN = "token_zp";
-// export const BASE_NAME = "https://res.cloudinary.com/dnio5vufj/"
+
 export const BASE_NAME = "https://res.cloudinary.com/ciberneto/"
 
